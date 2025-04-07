280e0468b8e714a8efb69e402ca0521bbdd287c9
Carlos Sac <carlos.sac@outlook.com>
2025-04-07 19:27:07 -0400

    Pass DetailedView Info

--- ios101-project6-tumblr/DetailViewController.swift
+++ ios101-project6-tumblr/DetailViewController.swift
@@ -7,12 +7,26 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+import Nuke
 
+class DetailViewController: UIViewController {
+    
+    var post : Post!
+
+    @IBOutlet weak var captionTextView: UITextView!
+    @IBOutlet weak var photoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        captionTextView.text = post.caption.trimHTMLTags()
+        
+        if let photo = post.photos.first {
+            let url = photo.originalSize.url
+            Nuke.loadImage(with: url, into: photoImageView)
+        }
+        
 
-        // Do any additional setup after loading the view.
+
     }
     
 
